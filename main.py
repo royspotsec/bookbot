56a65688c51b9490c630f1f8207f71dc85b97343
Muqtada Al-Saedi <76435059+royspotsec@users.noreply.github.com>
2025-11-17 14:05:36 +0300

    refactoring the code

--- main.py
+++ main.py
@@ -1,22 +1,20 @@
 import sys
 from stats import get_num_words
 from stats import count_chars
+
+
+
 print ("Usage: python3 main.py <path_to_book>")
 
+
 user_path = (sys.argv[1])
-
-
-
-
-        
 
 
 def get_book_text (filepath) :
     with open(filepath) as file :
         string_file = file.read()
     print (string_file)
-        
-   
+
 
 print (f"============ BOOKBOT ============")
 print (f"Analyzing book found at {user_path}")
